964ae9e6388c04ed011eeef9086b83501cbe7f41
michael <quizmastermike@gmail.com>
2026-06-20 18:40:22 -0700

    adding notes to mean2

--- mean/mean2.cpp
+++ mean/mean2.cpp
@@ -1,3 +1,13 @@
+/*
+
+In mean2 we introduce the concept of function overloading. 
+In c++ we can have multiple functions with the same name, which get identified by their input parameter types, not their return type.
+
+In this version of mean.cpp we can pass arguments to the program in the command line, as we invoke the program, like ./mean2 1 2 3 4
+
+*/
+
+
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
@@ -6,14 +16,14 @@ using namespace std;
 
 /*
   Instead of waiting for us to type the numbers into the console, we pass numbers as command line arguments i.e. using the run command: `./mean2 1 2 3 4`
-  
 */
 
-// in modern c++ you don't need to explicitly declare that a function is overloaded. So we removed the line: overload mean;
+// in modern c++ you don't need to explicitly declare that a function is overloaded. So we removed the next line: 
+// overload mean;
 double mean(double, char*[], int);
 long mean(long, char*[], int);
 
-int main(int len, char* buffer[]) //command line arguments setup. The compiler knows these are command lines by their position in the main function parameters.
+int main(int len, char* buffer[]) //command line arguments setup. The compiler knows these are command lines by their position & types in the main function parameters.
 {   
 
     if (len == 1) {
